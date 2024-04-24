1d5e8b2f643bfb74479295056fef7c0ae19f3bd3
JoiJaf <jafethrocha11@gmail.com>
2024-04-24 11:24:57 -0600

    project with react

---
+++ TaskFlow/src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header.jsx";
+//import {  } from "";
+//import {  } from "";
+
+import "./index.css";
+
+export function App() {
+
+  return (
+    <Header />
+  );
+}
---
+++ TaskFlow/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import "../index.css";
+
+export function Header() {
+    return (
+        <h1>Hola</h1>
+    )
+}
