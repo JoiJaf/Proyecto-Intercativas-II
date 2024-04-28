2e13a37c25836dab367c9f497779fefdde809697
Alpzar <andresalpizar53@yahoo.es>
2024-04-28 12:36:15 -0600

    Actualización
    se actualizó el index y app con todos los imports que se tienen hasta el momento

--- TaskFlow/src/App.jsx
+++ TaskFlow/src/App.jsx
@@ -1,5 +1,13 @@
 import { Header } from "./components/Header.jsx";
 import { Task } from "./components/TaskCarousel.jsx";
+import { Welcome } from "./components/Welcome.jsx";
+import { MyProgress } from "./components/MyProgress.jsx";
+import { Stadistics } from "./components/Stadistics.jsx";
+import { VisualElements } from "./components/VisualElements.jsx";
+import { Categories } from "./components/Categories.jsx";
+import { CreateAccount } from "./components/CreateAccount.jsx";
+import {Forms} from "./components/forms/Forms.jsx";
+import {Course} from "./components/Course.jsx";
 //import {  } from "";
 
 import "./index.css";
@@ -9,13 +17,25 @@ export function App() {
   return (
     <div>
       <Header />
-      <Task />
+      <Welcome />
+      <MyProgress />
+      <Stadistics />
+      <VisualElements/>
+      <Categories/>
+      <CreateAccount/>
     </div>
   );
 }
 //<Header />
+//<Welcome />
+//<MyProgress />
+//<Stadistics />
+//<VisualElements/>
+//<Categories/>
+//<CreateAccount/>
 //<Task />
 //<Forms />
+//<Course/>
 
 
 export default App;
