32c5e0400e410ef8d23cced46d47320625306215
AsuncionPD <asuncion.pena@ucr.ac.cr>
2024-05-02 22:44:37 -0600

    Added style to profile page
    Agregué estilo a la página y acomode los elementos

--- TaskFlow/src/components/profile/Profile.jsx
+++ TaskFlow/src/components/profile/Profile.jsx
@@ -4,24 +4,26 @@ export function Profile() {
 
     return (
         <section>
-            <h2>My profile</h2>
-            <section>                
-                <div>
-                    <img src="../src/assets/img/profile.png" alt="profile" />
+            <h2 className="mt-[38px] ml-[38px] clamp-xxl">My profile</h2>
+            <div class="flex justify-end mr-[38px]">
+                <span id="editText" class="text-blue-500">Edit</span>
+            </div>
+            <section className="grid grid-cols-2 gap-[38px]"> 
+            <section className="flex flex-wrap justify-center items-center border-[#EEEEEE] border-2 mb-[76px] mx-[38px] p-[38px] rounded-[20px]">                
+                <div className="my-[38px] mx-[72px]">
+                    <img className="w-[426px] h-[418px] rounded-full" src="../src/assets/img/profile.png" alt="profile" />
                 </div>
-                <h1>María Segura Umaña</h1>
-                <p>mariaumasegur@example.com</p>
-                <h3>Student</h3>
+                <div className="flex flex-col items-center">
+                    <h1 className="mt-[38px] clamp-xxl font-bold">María Segura Umaña</h1>
+                    <p className="text-[#A7A7A7] clamp-md">mariaumasegur@example.com</p>
+                    <h3 className="font-bold clamp-lg my-[72px]">Student</h3>
+                </div>
             </section>
 
-            <section>
-                <form method="post" action="#">
 
-                <div class="flex items-center">
-                    <span id="editText" class="text-blue-500">Edit</span>
-                </div>
-                
-                <div>
+            <form method="post" action="#">
+            <section className="border-[#EEEEEE] border-2 mb-[76px] mx-[38px] p-[38px] rounded-[20px]">
+                <div className="grid grid-cols-2 gap-8 text-[#A7A7A7]">
                     <div>
                         <div className="grid my-14">
                             <label htmlFor="age" className="clamp-md mb-1">Age</label>
@@ -58,8 +60,7 @@ export function Profile() {
                 </div>
 
                 <div>
-                    <div>
-
+                    <div className="grid grid-cols-2 gap-8 text-[#A7A7A7]">
                         <div className="grid my-14">
                             <label htmlFor="registration" className="clamp-md mb-1">Date of registration</label>
                             <input id="registration" type="text" name="registration" value="May 10th, 2023" className="form-input w-full h-[85px] rounded-[20px] text-black clamp-lg p-4 border-[#EEEEEE] border-2" />
@@ -71,9 +72,9 @@ export function Profile() {
                         </div>
                     </div>
                 </div>
-                </form>
-
             </section>
+            </form>
         </section>
+    </section>
     )
 }
