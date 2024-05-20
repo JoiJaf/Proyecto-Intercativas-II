dd80293787cb178dbdc6537ca6253a6e4b4025dd
AsuncionPD <asuncion.pena@ucr.ac.cr>
2024-05-20 15:06:58 -0600

    Implemented carousel function
    Se creó la función del carrusel con botones para poder dezplazar los componentes que contiene la información de las tareas

--- TaskFlow/src/components/dashboard/Carousel.jsx
+++ TaskFlow/src/components/dashboard/Carousel.jsx
@@ -1,20 +1,46 @@
-import "../../index.css";
+import React, { useRef } from 'react';
 import { TaskCard } from './TaskCard.jsx';
 import { tasks } from './DataTasks.jsx';
 
 export function Carousel() {
-  return (
-    <div className="flex gap-[2vw] cursor-pointer overflow-x-hidden w-full">
-        {tasks.map(task => (
-            <TaskCard
-                key={task.id}
-                bgColor={task.bgColor}
-                number={task.number}
-                title={task.title}
-                course={task.course}
-                dueDate={task.dueDate}
-            />
-        ))}
-    </div>
-    )
-  }
+    const scrollRef = useRef();
+
+    const handleScroll = (direction) => {
+        const container = scrollRef.current;
+        const scrollAmount = 200;
+        if (direction === 'left') {
+            container.scrollLeft -= scrollAmount;
+        } else {
+            container.scrollLeft += scrollAmount;
+        }
+    };
+
+    return (
+        <div className="flex gap-[2vw] cursor-pointer overflow-x-hidden w-full" ref={scrollRef}>
+            {tasks.map(task => (
+                <TaskCard
+                    key={task.id}
+                    bgColor={task.bgColor}
+                    number={task.number}
+                    title={task.title}
+                    course={task.course}
+                    dueDate={task.dueDate}
+                />
+            ))}
+            <button
+                className="absolute top-1/2 transform -translate-y-1/2 left-100 bg-white bg-opacity-25 hover:bg-opacity-85 rounded-full p-4"
+                onClick={() => handleScroll('left')}
+            >
+                &lt;
+            </button>
+            <button
+                className="absolute top-1/2 transform -translate-y-1/2 right-20 bg-white bg-opacity-25 hover:bg-opacity-85 rounded-full p-4"
+                onClick={() => handleScroll('right')}
+            >
+                &gt;
+            </button>
+        </div>
+    );
+}
+
+
